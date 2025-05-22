36d0c80e54faf38218a56a5f09a50e189fc2b558
Arn412 <rabhignan@gmail.com>
2025-05-22 19:40:47 +0530

    exported home function to module

---
+++ src/home.js
@@ -0,0 +1,9 @@
+export function home(){
+    const homepageContent = `
+<h1>Benvenuti to Saro's - Where Food Meets Fun!</h1>
+<p>At Saro's, we bring the heart of Italy to your plate. From handcrafted pastas to wood-fired pizzas, every dish is made with passion, tradition, and the freshest ingredients. Whether you're craving a cozy dinner for two or a festive gathering with friends, our warm atmosphere and rich flavors will make you feel right at home.<br>
+Come experience authentic Italian cuisine in a setting that feels like family. Buon appetito!</p>
+`;
+
+document.getElementById('content').innerHTML = homepageContent;
+};
